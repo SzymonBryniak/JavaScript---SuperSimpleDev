14c2b618a0adaffa13ab15d43e700e1660334db9
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-09 21:59:49 +0100

    exercises 11a - 11f completed

--- styles/scripts/11.1-exercises.js
+++ styles/scripts/11.1-exercises.js
@@ -1,12 +1,40 @@
-const nums = [10, 20, 30];
+const nums = [10, 20, 30];  //11a
 nums[2] = 99
 
 document.querySelector('.test').innerHTML = nums;
-console.log(nums)
+console.log(nums.length)
 
 
-function getLastValue(array){
-  console.log(array[array.lenght -1]);
+function getLastValue(numsArray){  //11b
+  let lastVal = numsArray.size;
+  
+  console.log(`This was the last value ${numsArray[nums.length - 1]}`);
 }
 
 getLastValue(nums);
+
+function arraySwap(array){  //11c
+  temp = array[array.length - 1];
+  array[-1] = array[0];
+  array[0] = temp;
+
+}
+
+for (let i = 0; i <= 10; i += 2) {  //d
+  console.log(i);
+}
+
+for (let i = 10; i >= 0; i -= 1) {  //e
+  console.log(i)
+}
+
+let i = 0;
+while (i != 10) {
+  i += 2;
+  console.log(i, i)
+}
+// let ii = 5;
+// while (i != 0){
+//   ii -= 1;
+//   console.log(ii, ii);
+// }
